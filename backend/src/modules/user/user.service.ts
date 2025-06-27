1670baf685a9d35754154c140b88e008ffd4af3d
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-06-27 10:31:18 -0300

    feat: add user modules basic rest structure

---
+++ backend/src/modules/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { PrismaService } from "src/shared/prisma/prisma.service";
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.user.findUnique();
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
