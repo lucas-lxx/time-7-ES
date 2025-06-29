d91b4b89d2e8a0b0317822d45d76d5eaaf7fd46c
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-06-29 18:50:47 -0300

    feat(auth/guard) register the auth.guard globaly

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module, ValidationPipe } from "@nestjs/common";
-import { APP_PIPE } from "@nestjs/core";
+import { APP_GUARD, APP_PIPE } from "@nestjs/core";
 import { PrismaModule } from "./shared/prisma/prisma.module";
 import { UserModule } from "./modules/user/user.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import { AuthGuard } from "./modules/auth/auth.guard";
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
@@ -10,6 +11,10 @@ import { AuthModule } from "./modules/auth/auth.module";
     {
       provide: APP_PIPE,
       useClass: ValidationPipe
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
     }
   ]
 })
