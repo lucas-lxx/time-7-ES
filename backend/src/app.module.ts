a84cf8054da379c003b7b003ffa53f259787e52c
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-06-29 14:44:33 -0300

    feat(app/auth): register AuthModule onto AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module, ValidationPipe } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
 import { PrismaModule } from "./shared/prisma/prisma.module";
 import { UserModule } from "./modules/user/user.module";
+import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   providers: [
     {
       provide: APP_PIPE,
