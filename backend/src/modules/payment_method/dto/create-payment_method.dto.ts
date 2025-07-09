9088c7c720ab3fa5523b443a1877918616789039
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-07-08 22:22:42 -0300

    payment

--- backend/src/modules/payment_method/dto/create-payment_method.dto.ts
+++ backend/src/modules/payment_method/dto/create-payment_method.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePaymentMethodDto {}
+import { IsString, MaxLength } from "class-validator";
+
+export class CreatePaymentMethodDto {
+  @IsString()
+  @MaxLength(255)
+  nome: string;
+    
+  @IsString()
+  classe_tipo_de_gasto: string ;
+}
--- backend/src/modules/payment_method/entities/payment_method.entity.ts
+++ backend/src/modules/payment_method/entities/payment_method.entity.ts
@@ -1,1 +1,14 @@
-export class PaymentMethod {}
+import { IsNotEmpty, IsString, IsUUID, MaxLength } from "class-validator";
+
+export class PaymentMethod {
+  @IsUUID()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @MaxLength(255)
+  nome: string;
+  
+  @IsString()
+  classe_tipo_de_gasto: string ;
+}
