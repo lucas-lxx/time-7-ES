425178a2dfd1ed1c7ef0891345d2b379d19c139d
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-07-09 17:51:39 -0300

    fix(user/service): fix the unique constraint error on email to return conflict

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,21 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { PrismaService } from "src/shared/prisma/prisma.service";
 import { hash } from "bcrypt";
 import { SALT_ROUNDS } from "src/shared/constants";
 import { UserWithoutPassword } from "./entities/user.entity";
+import { UserDto } from "./dto/user.dto";
+import { plainToClass } from "class-transformer";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<UserDto> {
     createUserDto.password = await hash(createUserDto.password, SALT_ROUNDS);
-    return this.prismaService.user.create({
-      select: UserWithoutPassword,
-      data: createUserDto
-    });
+    try {
+      const user = await this.prismaService.user.create({
+        select: UserWithoutPassword,
+        data: createUserDto
+      });
+      return plainToClass(UserDto, user);
+    } catch (err) {
+      if (err.code === "P2002") {
+        throw new ConflictException("Email is already in use");
+      }
+      throw new InternalServerErrorException();
+    }
   }
 
   async findAll() {
