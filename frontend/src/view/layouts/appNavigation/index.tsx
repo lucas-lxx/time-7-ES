acd50c0bb7506846b892d1d907231a9b938d4bec
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-14 20:43:13 -0300

    feat(Context): implementa o context api na aplicação

--- frontend/src/view/layouts/appNavigation/index.tsx
+++ frontend/src/view/layouts/appNavigation/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { LayoutSidebar } from './Sidebar';
+import { DashboardProvider } from '@/dashboard/DashboardContext';
 
 export default function AppNavigation() {
   return (
-    <>
+    <DashboardProvider>
       <LayoutSidebar>
         <Outlet />
       </LayoutSidebar>
-    </>
+    </DashboardProvider>
   );
 }
