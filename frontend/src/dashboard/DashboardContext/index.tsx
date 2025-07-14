f8841384218b804adbf6ae2bd2edb8d76ad2cd36
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-14 20:43:13 -0300

    feat(Context): implementa o context api na aplicação

---
+++ frontend/src/dashboard/DashboardContext/index.tsx
@@ -0,0 +1,34 @@
+import { createContext, useCallback, useState } from 'react';
+import { type organizationResponse } from '@/app/services/organizationService/getAll.ts';
+
+interface DashboardContextValue {
+  selectedOrganization: null | organizationResponse;
+  selectOrganization(org: organizationResponse): void;
+  clearSelectedOrganization(): void;
+}
+
+export const DashboardContext = createContext({} as DashboardContextValue);
+
+export function DashboardProvider({ children }: { children: React.ReactNode }) {
+  const [selectedOrganization, setSelectedOrganization] =
+    useState<null | organizationResponse>(null);
+
+  const selectOrganization = (org: organizationResponse) => {
+    setSelectedOrganization(org);
+    console.log('context:', { org });
+  };
+
+  const clearSelectedOrganization = () => setSelectedOrganization(null);
+
+  return (
+    <DashboardContext.Provider
+      value={{
+        clearSelectedOrganization,
+        selectOrganization,
+        selectedOrganization,
+      }}
+    >
+      {children}
+    </DashboardContext.Provider>
+  );
+}
