f085617730c9a1d53c09ecfe110ba630c2a7c122
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-15 12:09:45 -0300

    refactor: corrige a renderização em telas mobiles

--- frontend/src/dashboard/pages/minhasOrganizacoes/editOrganizacoes/components/EdicaoCard.tsx
+++ frontend/src/dashboard/pages/minhasOrganizacoes/editOrganizacoes/components/EdicaoCard.tsx
@@ -15,7 +15,7 @@ export default function EdicaoCard({ id }: EdicaoCardProp) {
 
   if (!selectedOrganization) {
     return (
-      <div className='absolute top-2 right-2 flex gap-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200'>
+      <div className='absolute top-2 right-2 flex gap-2 z-10 group-hover:opacity-100 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-200'>
         <button className='p-1 rounded hover:bg-red-100'>
           <Trash size={16} className='text-red-600' />
         </button>
