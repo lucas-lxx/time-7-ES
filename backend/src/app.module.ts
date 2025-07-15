7ec4f779c3650bfbc6a45675fd187e845afd0653
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-07-15 13:42:06 -0300

    chore(expense/swagger): add the swagger documentation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,21 @@ import { PrismaModule } from "./shared/prisma/prisma.module";
 import { UserModule } from "./modules/user/user.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { AuthGuard } from "./modules/auth/auth.guard";
-import { ExpenseClassModule } from './modules/expense_class/expense_class.module';
-import { PaymentMethodModule } from './modules/payment_method/payment_method.module';
+import { ExpenseClassModule } from "./modules/expense_class/expense_class.module";
+import { PaymentMethodModule } from "./modules/payment_method/payment_method.module";
 import { GroupModule } from "./modules/group/group.module";
-
+import { ExpenseModule } from "./modules/expense/expense.module";
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule, GroupModule, ExpenseClassModule, PaymentMethodModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    GroupModule,
+    ExpenseClassModule,
+    PaymentMethodModule,
+    ExpenseModule
+  ],
   providers: [
     {
       provide: APP_PIPE,
