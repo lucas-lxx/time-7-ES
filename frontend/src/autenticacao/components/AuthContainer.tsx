9b7f667fadebc2ae68bfccccd5220f3f1b323e5f
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-15 12:09:14 -0300

    refactor: implementa responsividade no cards de autenticação

--- frontend/src/autenticacao/components/AuthContainer.tsx
+++ frontend/src/autenticacao/components/AuthContainer.tsx
@@ -5,7 +5,7 @@ interface AuthContainerProps {
 
 export function AuthContainer({ children, titulo }: AuthContainerProps) {
   return (
-    <div className='flex flex-col items-center bg-white px-10 pt-8 rounded-md min-h-[26rem] h-auto max-w-105'>
+    <div className='flex flex-col items-center bg-white px-4 pt-8 w-full max-w-[350px] rounded-md min-w-[20rem] min-h-[26rem] h-auto'>
       <h1 className='text-cor-principal text-3xl font-normal font-roboto mb-6'>
         {titulo}
       </h1>
