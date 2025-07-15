d7eec255658fc510f04992a048b2c2e0993f6f51
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-14 22:40:58 -0300

    refactor: mudança no texto

--- frontend/src/dashboard/pages/minhasOrganizacoes/index.tsx
+++ frontend/src/dashboard/pages/minhasOrganizacoes/index.tsx
@@ -8,7 +8,7 @@ export default function MinhasOrganizacoes() {
     <div className='w-full h-full space-y-10 p-2 bg-[#F6F6F6] rounded-xl pt-4 sm:pt-6 md:pt-8 lg:pt-12'>
       <div className='flex max-sm:flex-col  sm:justify-between sm:items-center'>
         <h1 className='font-roboto font-semibold pl-7 text-lg sm:text-2xl md:text-3xl lg:text-4xl'>
-          Minhas Organizações
+          Meus Grupos
         </h1>
         <SheetButton
           nomeEvento='Criar Organização'
