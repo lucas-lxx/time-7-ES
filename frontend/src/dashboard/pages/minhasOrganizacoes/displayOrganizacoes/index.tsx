f840842f504e7d717fdecbe47519fb710d86a91d
Yuri_Oliveira <oliveiracc@alu.ufc.br>
2025-07-14 21:16:49 -0300

    feat: cria estado vazio para grupos com imagem ilustrativa

--- frontend/src/dashboard/pages/minhasOrganizacoes/displayOrganizacoes/index.tsx
+++ frontend/src/dashboard/pages/minhasOrganizacoes/displayOrganizacoes/index.tsx
@@ -1,22 +1,22 @@
 import CardOrganizacao from './components/CardOrganizacao';
 import { useMyOrganizationsController } from './useMyOrganizationsController';
 import { PageLoader } from '@/view/components/PageLoader';
+import { PageEmptyGroup } from '@/view/components/PageEmptyGroup';
 
 export default function DisplayOrganizacoes() {
   const { isLoading, myOrganizations } = useMyOrganizationsController();
 
   if (isLoading) return <PageLoader />;
+
   return (
     <div className='flex flex-wrap px-4 gap-x-4 gap-y-6 max-sm:gap-y-1 max-sm:h-[85%] h-[90%] '>
-      {myOrganizations.map((org) => (
-        <CardOrganizacao
-          key={org.id}
-          id={org.id}
-          title={org.name}
-          description={org.description}
-          date={'12/12/2024'}
-        />
-      ))}
+      {myOrganizations.length === 0 ? (
+        <PageEmptyGroup />
+      ) : (
+        myOrganizations.map((org) => (
+          <CardOrganizacao key={org.id} organizacao={org} />
+        ))
+      )}
     </div>
   );
 }
